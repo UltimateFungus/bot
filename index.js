40e227ede9be4b051da4b349dfb3ea4472a43eb5
WhipLash <ultifungus@gmail.com>
2025-07-07 20:55:59 +0200

    using basic headers

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const data = await fetch("https://api.moomoo.io/verify");
+const headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+  'Accept-Language': 'en-US,en;q=0.9',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,/;q=0.8',
+  'Connection': 'keep-alive',
+  'Upgrade-Insecure-Requests': '1'
+};
+
+const data = await fetch('https://api.moomoo.io/verify', { headers });
 console.log(data);
