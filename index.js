6e7f7cd5ca34105b875c9a4e0156e1f9e659f6b8
UltimateFungus <119760579+UltimateFungus@users.noreply.github.com>
2025-08-01 10:05:30 +0000

    v1

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import { solveChallenge, solveChallengeWorkers } from "altcha-lib";
-
+import WebSocket from "ws";
 async function solve(data) {
-    const result = await solveChallenge(
+    const result = await solveChallengeWorkers(
+        "./node_modules/altcha-lib/dist/worker.js",
+        8,
         data.challenge,
         data.salt,
         data.algorithm,
         data.maxnumber
-    ).promise;
+    );
     const tokenObj = JSON.stringify({
       algorithm: data.algorithm,
       challenge: data.challenge,
@@ -31,9 +33,13 @@ async function challenge() {
 challenge();
 class Bot {
 	constructor(name, token) {
-		this.ws = new WebSocket(`${thisServerAddress}/?token=${encodeURIComponent(`alt:${token}`)}`);
+		this.ws = new WebSocket(`${thisServerAddress}/?token=${encodeURIComponent(`alt:${token}`)}`, {
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            }
+        });
         this.ws.onopen = e => console.log("open");
         this.ws.onclose = e => console.log(e);
-        this.ws.onerror = e => console.log(e);
+        this.ws.onerror = e => console.log(e._closeCode);
 	}
 }
